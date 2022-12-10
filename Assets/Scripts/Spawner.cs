ade4903125007bb5975e0a6160c64499e27dc782
Zach Draper <zdraper@gmail.com>
2022-12-09 21:28:02 -1000

    Changed some variable names in the Spawner script.

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -8,13 +8,13 @@ public class Spawner : MonoBehaviour
     [SerializeField] private float _spawnIntervalMax = 5f;
     [SerializeField] private float _spawnIntervalMin= .1f;
 
-    [SerializeField] private float _spawnPointY;
+    [SerializeField] private float _yDistanceFromPlayer;
     [SerializeField] private GameObject _enemy;
     [SerializeField] private GameObject _player;
-    [SerializeField] private float _spawnOffset;
+    [SerializeField] private float _xMaxDistanceFromPlayer;
     [SerializeField] private GameObject _enemyContainer;
 
-    private bool isPlayerDead;
+    private bool _isPlayerDead;
     void Start()
     {
        StartCoroutine(SpawnEnemiesRoutine());
@@ -30,12 +30,12 @@ public class Spawner : MonoBehaviour
     {
         var playerScript = _player.GetComponent<Player>();
         
-        while(isPlayerDead == false)
+        while(_isPlayerDead == false)
         {
             float spawnInterval = Random.Range(_spawnIntervalMin, _spawnIntervalMax);
             var playerPosition = _player.transform.position;
-            float randomX = Random.Range(playerPosition.x - _spawnOffset, playerPosition.x + _spawnOffset);
-            Vector3 randomSpawnPoint = new Vector3(Mathf.Clamp(randomX, -9f, 9f), _spawnPointY, 0f);
+            float randomX = Random.Range(playerPosition.x - _xMaxDistanceFromPlayer, playerPosition.x + _xMaxDistanceFromPlayer);
+            Vector3 randomSpawnPoint = new Vector3(Mathf.Clamp(randomX, -9f, 9f), _yDistanceFromPlayer, 0f);
             GameObject newEnemy = Instantiate(_enemy, randomSpawnPoint, Quaternion.identity);
             newEnemy.transform.SetParent(_enemyContainer.transform);
             yield return new WaitForSeconds(spawnInterval);
@@ -44,6 +44,6 @@ public class Spawner : MonoBehaviour
 
     public void onPlayerDeath()
     {
-        isPlayerDead = true;
+        _isPlayerDead = true;
     }
 }
